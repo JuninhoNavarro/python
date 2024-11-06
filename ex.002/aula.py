955f37c4732497bd4c4fed2d035728a64b4acb6d
JoseNavarroAlvesJunior <juninho.josealves131@gmail.com>
2024-11-06 18:27:20 -0300

    2ª aula - informe um número

---
+++ ex.002/aula.py
@@ -0,0 +1,9 @@
+#Faça um Programa que peça um número e então mostre a mensagem "O número informado foi [número].""
+
+
+numero: float # Declaração da variável "numero" do tipo float.
+
+numero = float(input("Informe um número: "))  # Converte a entrada para float
+
+# Exibe a mensagem com o número informado pelo usuário
+print(f'O número informado é {numero}.')  # Mostra o número informado
