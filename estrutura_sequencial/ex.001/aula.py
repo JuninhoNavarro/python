02ee489aa63ed8f31eab08e48fa938a1579ddc1b
JoseNavarroAlvesJunior <juninho.josealves131@gmail.com>
2024-11-06 18:37:28 -0300

    1ª aula (alo mundo)

--- estrutura_sequencial/ex.001/aula.py
+++ estrutura_sequencial/ex.001/aula.py
@@ -1,2 +1,3 @@
 #Faça um Programa que mostre a mensagem "Alo mundo" na tela.
-print("alo mundo") # usada para exibir informações na tela.
+
+print("alo mundo") # o (print) é usado para exibir informações na tela.
