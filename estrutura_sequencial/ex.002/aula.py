6bf093bb504e3f2bfd9a97448fc2d65910f98f91
JoseNavarroAlvesJunior <juninho.josealves131@gmail.com>
2024-11-06 18:40:22 -0300

    2ª aula (informe um número)

--- estrutura_sequencial/ex.002/aula.py
+++ estrutura_sequencial/ex.002/aula.py
@@ -4,6 +4,7 @@
 numero: float # Declaração da variável "numero" do tipo float.
 
 numero = float(input("Informe um número: "))  # Converte a entrada para float
+# (input) é a entrada que o usuario fornece
 
 # Exibe a mensagem com o número informado pelo usuário
-print(f'O número informado é {numero}.')  # Mostra o número informado
+print(f'O número informado é {numero}.')  # (print) Mostra o número informado
