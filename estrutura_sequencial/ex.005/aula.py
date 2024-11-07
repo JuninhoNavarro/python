f2bc440f06684a8a9f4c8f8f4119ceeb877e912b
JoseNavarroAlvesJunior <juninho.josealves131@gmail.com>
2024-11-07 17:20:34 -0300

    5ª aula (converter metros em centímetros)

---
+++ estrutura_sequencial/ex.005/aula.py
@@ -0,0 +1,16 @@
+# Faça um Programa que converta metros para centímetros.
+
+# Declara a variável medida_metros como tipo float
+medida_metros = float 
+
+# Solicita ao usuário que informe a medida em metros e converte para float
+medida_metros = float(input("Informe a medida em metros: "))
+
+# Declara a variável medida_centimetros como tipo float
+medida_centimetros = float
+
+# Converte a medida de metros para centímetros multiplicando por 100
+medida_centimetros = medida_metros * 100
+
+# Exibe o resultado da conversão de metros para centímetros
+print(f'{medida_metros} metros convertendo em centímetros é {medida_centimetros} centímetros.')
