84b92c34ef401c6458934abdb38c3e5f31aff954
JoseNavarroAlvesJunior <juninho.josealves131@gmail.com>
2024-11-07 17:23:32 -0300

    6ª aula (calcular area de um círculo)

---
+++ estrutura_sequencial/ex.006/aula.py
@@ -0,0 +1,20 @@
+# Faça um Programa que peça o raio de um círculo, calcule e mostre sua área.
+
+# Declara a variável raio como tipo float
+raio = float
+
+# Solicita ao usuário que informe o raio do círculo e converte o valor para float
+raio = float(input('Informe o raio do círculo: '))
+
+# Declara a variável area como tipo float
+area = float
+
+# Declara a constante pi como tipo float e atribui o valor aproximado de pi
+pi = float 
+pi = 3.14
+
+# Calcula a área do círculo utilizando a fórmula: área = pi * raio^2
+area = raio**2 * pi
+
+# Exibe o resultado da área calculada para o usuário
+print(f'A área do círculo de raio {raio} é de: {area}. ')
